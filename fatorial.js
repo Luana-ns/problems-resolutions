49eabfd3b28486c5751c27a6eb55b75c124dd9f9
Luana Nunes <lununes.s@outlook.com>
2021-04-22 09:25:23 -0300

    Atualizando fatoriais

--- fatorial.js
+++ fatorial.js
@@ -1,5 +1,8 @@
 //Dado n = 5, 5! é igual a: 
-let fatorial = function fat(n){
-    return n<2 ? 1 : n*fat(n-1) 
-    }
-    console.log(fatorial(5))
+function fatorial(n){
+    if ((n == 0) || (n == 1))
+        return 1;
+    else 
+        return (n * fatorial(n - 1));
+}
+console.log(fatorial(5))
