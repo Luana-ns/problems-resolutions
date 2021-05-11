e5144dcc3fb885b84cc8d9f7b0968a7a07ea007a
Luana Nunes <lununes.s@outlook.com>
2021-05-11 17:28:57 -0300

    Aprimorando resoluções e corrigindo erros

--- lendoLivros.js
+++ lendoLivros.js
@@ -1,11 +1,14 @@
 // Resolução do exercício que se encontra no site: https://www.urionlinejudge.com.br/judge/pt/problems/view/1542
+// q = quantidade de páginas lidas por dia
+// d = dias adiantados se forem lidas p páginas por dia
+// p = número maior de páginas que poderiam ser lidas por dia
 
-function lendoLivros( quantidadeDePaginasLidas, diasAdiantadosSeFossemLidasMaisPaginas, paginasQuePoderiamSerLidas) {
-    totalDePaginasDoLivro = (quantidadeDePaginasLidas * diasAdiantadosSeFossemLidasMaisPaginas * paginasQuePoderiamSerLidas)/(paginasQuePoderiamSerLidas - quantidadeDePaginasLidas)
 
-    if (totalDePaginasDoLivro == 1){
-        console.log(`${totalDePaginasDoLivro} pagina`)
-    } else console.log(`${totalDePaginasDoLivro} paginas`)
+function calculatePages(pagesRead, days, pagesThatCouldBeRead) {
+    const q = pagesRead
+    const d = days
+    const p = pagesThatCouldBeRead
+    totalPages = (q * d * p)/(p - q)
+    return totalPages
 }
-// Teste: 
-lendoLivros(3, 16, 5)
+console.log(`${calculatePages(3, 16, 5)} página(s)`)
--- parOuImpar.js
+++ parOuImpar.js
@@ -4,13 +4,14 @@ function playWinner (players, play){
     } 
     return players[1]
 } 
-const testCase = [[['Pedro', 'Paulo'], ['Claudio', 'Carlos']], [[[2 , 4] , [ 3 , 5] , [1 , 0]] , [[1 , 5] , [2 , 3]]]]
+const testCase = [[['Pedro', 'Paulo'], ['Claudio', 'Carlos'], ['Luana', 'Filipe']], 
+                [[[2 , 4] , [ 3 , 5] , [1 , 0]] , [[1 , 5] , [2 , 3]], [[7, 7], [8, 7], [0, 0], [3, 2]]]]
 for (let i = 0; i < testCase.length -1; i++){
     for (let j = 0; j < testCase[1].length; j++){   
         console.log(`Teste ${j + 1}`)
         for (let k = 0; k < testCase[1][j].length; k++ ){
             console.log(playWinner(testCase[0][j],testCase[1][j][k]))
         } 
-        console.log(' ')  
+        console.log( )  
     }  
 } 
