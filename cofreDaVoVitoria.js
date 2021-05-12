698e557cc2ad726dc530f2ce0a62379af857a5a0
Luana Nunes <lununes.s@outlook.com>
2021-05-11 23:03:35 -0300

    Rascunho inicial do exercício: Cofre da vó Vitória

---
+++ cofreDaVoVitoria.js
@@ -0,0 +1,19 @@
+function safeBox (valorJoao, valorZe){
+    let result = 0
+    result = (valorJoao + valorZe) 
+    return result
+}
+const testCase = [[[20 , 25] , [10 , 5] , [10 , 10]] , [[0 , 5] , [12 , 0] , [0 , 20], [17, 1]]]
+
+
+for (let i = 0; i < testCase.length -1; i++){
+    for (let j = 0; j < testCase[i].length -1; j++){   
+        console.log(`Teste ${j + 1}`)
+        for (let k = 0; k < testCase[i][j].length; k++ ){
+            let saldo = testCase[i][j][k]+testCase[i][(j+1)][k]
+            console.log(safeBox(saldo))
+            }
+        
+        console.log(safeBox(testCase))
+    }
+}
