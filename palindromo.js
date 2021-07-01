55a5e9f9fe6fd686e3d401742e3bb9d86bd0a8df
Luana Nunes <lununes.s@outlook.com>
2021-07-01 19:16:48 -0300

    Atualizando resolução do exercício

--- palindromo.js
+++ palindromo.js
@@ -1,18 +1,11 @@
 // Dada uma palavra, verificar com o mínimo de interações possíveis, se ela é um palíndromo.
 // testCase: [ovo, amor a roma]
 
-//const testCase = 'ovo'
-
 function checkPalindromo (word) {
     for (let i = 0; i < word.length / 2; i++){
-        for (let z = 0; z < word.length /2; z--){
-            if (word[i] == word[z]){
-                return true
-            } console.log('É um palíndromo')
-        }
-         
-    }
-    
-}
-
-checkPalindromo('rua')
+        if (word[i] != word[word.length - i - 1]){
+            return false
+        }else return true
+    }  
+} 
+console.log(checkPalindromo('amor a roma'))
