f48edc276f08f12e88481d982b59a1c031b2cc41
Luana Nunes <lununes.s@outlook.com>
2021-07-27 20:14:50 -0300

    Adicionando exercício Diferença

---
+++ diferenca.js
@@ -0,0 +1,10 @@
+function difference(a, b, c, d){
+    let difference = (a * b - c * d)
+    return difference
+}
+x = difference (5, 6, 7, 8);
+y = difference (0, 0, 7, 8);
+z = difference (5, 6, -7, 8);
+console.log(`DIFERENCA = ${x}
+DIFERENCA = ${y}
+DIFERENCA = ${z}`);
