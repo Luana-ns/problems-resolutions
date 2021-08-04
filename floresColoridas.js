51705973c6903e116b783f38ba4088d68ec08b0b
Luana Nunes <lununes.s@outlook.com>
2021-08-04 19:07:45 -0300

    Adicionando algumas variáveis

--- floresColoridas.js
+++ floresColoridas.js
@@ -7,3 +7,14 @@ function triangulo(a, b, c){
     return 1;
 }
 
+function areasColoridas(){
+    if (triangulo(lado1,lado2,lado3) == 1){
+        let p = (lado1 + lado2 + lado3) / 2;
+        let areaTri = Math.sqrt(p*(p-lado1)*(p-lado2)*(p*lado3));
+        const pi = 3.1415926535897;
+        let raioCircInscrita = (areaTri / p);
+        let areaCircInscrita = pi * raioCircInscrita * raioCircInscrita;
+        let raioCircCircunscrita = (lado1 * lado2 * lado3) / (4 * areaTri);
+        let areaCircCircunscrita = pi * raioCircCircunscrita * raioCircCircunscrita;
+    }
+}
